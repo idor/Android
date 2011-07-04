3e438b4fd5ddf3e3a88f6beb3ade59d4ca3f5ff2
Ido Reis <idor@ti.com>
2011-07-04 23:13:30 +0300

    udhcpd task now running in background during script run

--- WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
+++ WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
@@ -3,4 +3,4 @@
 insmod /system/lib/modules/wl12xx_sdio.ko
 hostapd_bin -B /data/misc/wifi/ap/hostapd.conf
 ifconfig wlan0 192.168.43.1 up
-udhcpd -f /data/misc/wifi/ap/udhcpd.conf
+udhcpd -f /data/misc/wifi/ap/udhcpd.conf &
