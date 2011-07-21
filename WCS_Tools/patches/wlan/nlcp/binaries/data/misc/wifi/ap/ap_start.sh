aaa5bc8781afdac367806b6d6b5e169a1212d8ec
Ido Reis <idor@ti.com>
2011-07-21 21:12:46 +0300

    initial_regdom service is executed before starting driver
    A band hostap had failed every time it was executed after reboot since 'world' regulatory domain does not allow A band.
    Once the initialregdom script is called after boot with specific country regulatory, it works fine.

--- WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
+++ WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
@@ -1,7 +1,8 @@
 #!/system/bin/sh
-
+setprop ctl.start initial_regdom
 insmod /system/lib/modules/wl12xx_sdio.ko
 /system/bin/logwrapper /system/bin/hostapd_bin -dd -B /data/misc/wifi/ap/hostapd.conf
 sleep 2
 ifconfig wlan0 192.168.43.1 netmask 255.255.255.0 up
+sleep 2
 udhcpd -f /data/misc/wifi/ap/dhcpd.conf &
