7ff2562eb2d4c2e97bc0109ed1445a9e6eb1cef0
Ido Reis <idor@ti.com>
2011-07-25 12:07:43 +0300

    regulatory domain alignment to hostapd.conf

--- WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
+++ WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
-setprop ctl.start initial_regdom
 insmod /system/lib/modules/wl12xx_sdio.ko
+iw reg set `grep country_code= /data/misc/wifi/ap/hostapd.conf | sed "s:country_code=::"`
+iw reg get
 /system/bin/logwrapper /system/bin/hostapd_bin -dd -B /data/misc/wifi/ap/hostapd.conf
 sleep 2
 ifconfig wlan0 192.168.43.1 netmask 255.255.255.0 up
