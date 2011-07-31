dd7739341ac19d84f42daf25336cf003c9bafa5b
Ido Reis <idor@ti.com>
2011-07-31 18:22:55 +0300

    added some remarks in the ap start/stop scripts

--- WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
+++ WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_start.sh
@@ -7,3 +7,14 @@ sleep 2
 ifconfig wlan0 192.168.43.1 netmask 255.255.255.0 up
 sleep 2
 udhcpd -f /data/misc/wifi/ap/dhcpd.conf &
+
+
+
+#insmod /system/lib/modules/wl12xx_sdio.ko
+#iw wlan0 del
+#iw `ls /sys/class/ieee80211/` interface add wlan1 type managed
+#setprop ctl.start hostapd_bin
+#sleep 2
+#setprop ctl.start ifcfg_softap
+#sleep 2
+#setprop ctl.start dhcpd_softap
--- WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_stop.sh
+++ WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/ap/ap_stop.sh
@@ -6,3 +6,12 @@ killall logwrapper
 killall hostapd_bin
 sleep 2
 rmmod wl12xx_sdio
+
+#setprop ctl.stop dhcpd_softap
+#ifconfig wlan1 down
+#sleep 1
+#setprop ctl.stop hostapd_bin
+#sleep 2
+#rmmod wl12xx_sdio
+
+
