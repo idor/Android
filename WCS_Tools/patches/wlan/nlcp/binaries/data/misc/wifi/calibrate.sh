458d38663b61bc0ab217b218f6b8693150f5bf47
Ido Reis <idor@ti.com>
2011-08-08 20:02:01 +0300

    calibrate.sh: update script for NVS 2.1

--- WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/calibrate.sh
+++ WCS_Tools/patches/wlan/nlcp/binaries/data/misc/wifi/calibrate.sh
@@ -23,36 +23,54 @@
 
 #!/system/bin/sh
 
-export NEW_MAC_ADDRESS=$1
-export INI_FILE=$2
+echo "Going to stop GUI"
+stop
 
-#if [ "$NEW_MAC_ADDRESS" == "" ] ; then export NEW_MAC_ADDRESS=08:00:28:12:34:56 ; fi
-#if [ "$INI_FILE" == "" ] ; then export INI_FILE=/system/etc/wifi/ini_files/128x/TQS_D_1.7.ini ; fi
 
-echo Create reference NVS
-calibrator set ref_nvs $INI_FILE
+rmmod wl12xx_sdio
+rmmod wl12xx
+rmmod mac80211
+rmmod cfg80211
+rmmod compat
+insmod /system/lib/modules/compat.ko
+insmod /system/lib/modules/cfg80211.ko
+insmod /system/lib/modules/mac80211.ko
 
-echo Copy reference NVS file
+echo "Create reference NVS"
+calibrator set ref_nvs $1
+
+echo "Copy reference NVS file"
 cat ./new-nvs.bin > /system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 
-echo Insert wl12xx SDIO module
+echo "Insert wl12xx SDIO module"
+insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl12xx_sdio.ko
 
-echo Calibrate device
+echo "Calibrate device"
 calibrator wlan0 plt power_mode on
 calibrator wlan0 plt tune_channel 0 7
+calibrator phy0 plt nvs_ver wlan0
 calibrator wlan0 plt tx_bip 1 1 1 1 1 1 1 1
 calibrator wlan0 plt power_mode off
 
-echo Set MAC address in NVS file $NEW_MAC_ADDRESS
-calibrator set nvs_mac ./new-nvs.bin $NEW_MAC_ADDRESS
-#calibrator set nvs_mac ./new-nvs.bin 08:00:28:00:64:87
-
-echo Remove wl12xx modules
-rmmod wl12xx_sdio wl12xx
+echo "Remove wl12xx modules"
+rmmod wl12xx_sdio
 rmmod wl12xx
 
-echo Copy calibrated NVS file
+echo "Set MAC address in NVS file"
+calibrator set nvs_mac ./new-nvs.bin $2
+
+echo "Copy calibrated NVS file"
 cat ./new-nvs.bin > /system/etc/firmware/ti-connectivity/wl1271-nvs.bin
+
+rmmod mac80211
+rmmod cfg80211
+rmmod compat
+insmod /system/lib/modules/compat.ko
+insmod /system/lib/modules/cfg80211.ko
+insmod /system/lib/modules/mac80211.ko
 insmod /system/lib/modules/wl12xx.ko
 
+echo "Going to start GUI"
+start
+#
